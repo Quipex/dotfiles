fa2beb314d0572671ac08c215af1943155dceaec
Dmytro Rudas <dmytro.rudas@revolut.com>
2025-06-16 16:50:16 +0200

    checkpoint: make wezterm alt + left/right work

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,30 +1,21 @@
 local wezterm = require 'wezterm'
 
+local act = wezterm.action
+
 local config = {
     adjust_window_size_when_changing_font_size = false,
     enable_tab_bar = false,
     font_size = 14.0,
     font = wezterm.font('JetBrains Mono'),
     macos_window_background_blur = 30,
-
-    -- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-    -- window_background_image_hsb = {
-    -- 	brightness = 0.01,
-    -- 	hue = 1.0,
-    -- 	saturation = 0.5,
-    -- },
     window_background_opacity = 0.92,
-    -- window_background_opacity = 1.0,
-    -- window_background_opacity = 0.78,
-    -- window_background_opacity = 0.20,
-    -- window_decorations = 'RESIZE',
     keys = {{
         key = "LeftArrow",
-        mods = "ALT",
-        action = act.SendString("b")
+        mods = "OPT",
+        action = act.SendString("\x1bb")
     }, {
         key = "RightArrow",
-        mods = "ALT",
+        mods = "OPT",
         action = act.SendString("\x1bf")
     }},
     mouse_bindings = { -- Ctrl-click will open the link under the mouse cursor
