7d053c6fdb036bc2c5aa632fc19fe050350168b3
Quipex <rudas72@gmail.com>
2025-06-22 23:18:33 +0200

    add monitor to the workspace indication

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -4,4 +4,36 @@ if [[ -z "$FOCUSED_WORSPACE" ]]; then
     FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 fi
 
-sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE"
+update_space() {
+    FILTER='.[0]."monitor-name"'
+    MONITOR_NAME=$(aerospace list-monitors --focused --json | jq -r "$FILTER")
+
+    case $MONITOR_NAME in
+    "Built-in Retina Display")
+        ICON=
+        ICON_PADDING_LEFT=7
+        ICON_PADDING_RIGHT=7
+        LOCALIZED_NAME='Native'
+        ;;
+    "Q27G3XMN")
+        ICON=󰍹
+        ICON_PADDING_LEFT=7
+        ICON_PADDING_RIGHT=7
+        LOCALIZED_NAME='AOC'
+        ;;
+    *)
+        ICON=󰍹
+        ICON_PADDING_LEFT=7
+        ICON_PADDING_RIGHT=7
+        LOCALIZED_NAME=$MONITOR_NAME
+    esac
+
+    sketchybar --set $NAME \
+        icon=$ICON \
+        icon.padding_left=$ICON_PADDING_LEFT \
+        icon.padding_right=$ICON_PADDING_RIGHT
+}
+
+update_space
+
+sketchybar --set $NAME.name label="$FOCUSED_WORKSPACE"
