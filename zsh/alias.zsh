a9f880411406c112263ee5c73a43f208d5901d55
Quipex <rudas72@gmail.com>
2025-08-08 19:14:20 +0200

    some updates

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,14 +1,14 @@
 # Aliases
-alias lpc='{{ .chezmoi.homeDir }}/scripts/list-path-content.sh'
-alias dockerup='{{ .chezmoi.homeDir }}/scripts/start-docker.sh'
-alias tg-alert='{{ .chezmoi.homeDir }}/scripts/tg-alert.sh'
-alias tmpclear='rm -rf {{ .chezmoi.homeDir }}/tmp/*'
-alias wpc='{{ .chezmoi.homeDir }}/scripts/write-path-content.sh'
-alias sourcezsh='source ~/.zshrc'
-alias wdiff='git diff master...HEAD > {{ .chezmoi.homeDir }}/tmp/git-diff.txt'
+alias lpc="$HOME/scripts/list-path-content.sh"
+alias dockerup="$HOME/scripts/start-docker.sh"
+alias tg-alert="$HOME/scripts/tg-alert.sh"
+alias tmpclear="rm -rf $HOME/tmp/*"
+alias wpc="$HOME/scripts/write-path-content.sh"
+alias sourcezsh="source ~/.zshrc"
+alias wdiff="git diff master...HEAD > $HOME/tmp/git-diff.txt"
 alias tokenize='python3 -c "import sys,tiktoken; print(len(tiktoken.get_encoding(\"cl100k_base\").encode(sys.stdin.read())))"'
 
 # Misc
-alias uncommit='git reset --soft HEAD^'
-alias ynab-convert='{{ .chezmoi.homeDir }}/Documents/Coding/ynab-imports-automation/convert-csv.sh'
+alias uncommit="git reset --soft HEAD^"
+alias ynab-convert="$HOME/Documents/Coding/ynab-imports-automation/convert-csv.sh"
 
