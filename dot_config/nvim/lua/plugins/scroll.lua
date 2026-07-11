6be62dd136b623e3837ba0c246498085549d3df5
Quipex <rudas72@gmail.com>
2026-07-11 16:57:36 +0200

    Fix cinnamon.nvim config: use non-deprecated options

--- dot_config/nvim/lua/plugins/scroll.lua
+++ dot_config/nvim/lua/plugins/scroll.lua
@@ -3,9 +3,23 @@ return {
     'declancm/cinnamon.nvim',
     lazy = false,
     opts = {
-      keyframes = 180,       -- duration in ms
-      max_length = 500,      -- don't animate very long jumps
-      scroll_limit = 150,    -- max lines to animate
+      keymaps = {
+        basic = true,   -- smooth Ctrl-D/Ctrl-U, G/gg, etc.
+        extra = true,   -- smooth n/N, */#, %, etc.
+      },
+      options = {
+        delay = 5,          -- ms between steps
+        step_size = {
+          vertical = 1,
+          horizontal = 2,
+        },
+        max_delta = {
+          line = 150,       -- max lines to animate
+          column = false,   -- no column limit
+          time = 1000,      -- max animation duration in ms
+        },
+        mode = "cursor",    -- scroll cursor smoothly
+      },
     },
   },
 }
